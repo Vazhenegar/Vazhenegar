6762f6002d2a2dc5d5bd81bf6709d01f6bc361aa
vazhenegar <vazhenegar@hotmail.com>
2020-01-13 20:08:00 +0330

    added orders table

--- database/migrations/2019_10_12_000000_create_users_table.php
+++ database/migrations/2019_10_12_000000_create_users_table.php
@@ -14,7 +14,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->bigIncrements('id')->unsigned();
             $table->string('FirstName');
             $table->string('LastName');
             $table->string('BirthDate')->nullable();
---
+++ database/migrations/2020_01_13_143445_create_orders_table.php
@@ -0,0 +1,72 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('UserId')->unsigned();
+            $table->timestamp('RegisterDate')->useCurrent();
+            $table->timestamp('DeliveryDate')->nullable();
+            $table->tinyInteger('RelatedDepartment')->unsigned();
+            $table->bigInteger('SourceLanguage')->unsigned()->nullable();
+            $table->bigInteger('DestLanguage')->unsigned()->nullable();
+            $table->bigInteger('TranslationField')->unsigned()->nullable();
+            $table->integer('Amount')->nullable(); //number of words to be translated
+            $table->decimal('TotalPrice',12,3)->nullable();
+            $table->decimal('PrePaidPrice',12,3)->nullable();
+            $table->bigInteger('ResponsibleUserId')->unsigned()->nullable();
+            $table->tinyInteger('StatusId')->unsigned()->nullable();
+            $table->longText('Description')->nullable();
+            $table->timestamps();
+
+            $table->foreign('UserId')
+                ->references('id')->on('users')
+                ->onUpdate('cascade')->onDelete('cascade');
+
+            $table->foreign('RelatedDepartment')
+                ->references('id')->on('departments')
+                ->onUpdate('cascade')->onDelete('cascade');
+
+            $table->foreign('SourceLanguage')
+                ->references('id')->on('languages')
+                ->onUpdate('cascade')->onDelete('cascade');
+
+            $table->foreign('DestLanguage')
+                ->references('id')->on('languages')
+                ->onUpdate('cascade')->onDelete('cascade');
+
+            $table->foreign('TranslationField')
+                ->references('id')->on('translation_fields')
+                ->onUpdate('cascade')->onDelete('cascade');
+
+            $table->foreign('ResponsibleUserId')
+                ->references('id')->on('users')
+                ->onUpdate('cascade')->onDelete('cascade');
+
+            $table->foreign('StatusId')
+                ->references('id')->on('order_statuses')
+                ->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
